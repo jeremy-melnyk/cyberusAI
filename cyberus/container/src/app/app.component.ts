ad5e7dc6e81ea20cfecb969014fdc418030fddf0
Jeremy Melnyk <jeremyjm91@gmail.com>
2017-09-16 18:16:10 -0400

    Changed view css.

--- cyberus/container/src/app/app.component.ts
+++ cyberus/container/src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css', '../../node_modules/videogular2/fonts/videogular.css']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'Welcome to CyberusAI';
