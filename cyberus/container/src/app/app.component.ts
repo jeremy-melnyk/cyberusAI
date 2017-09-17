e21661a2b195e42001a5df22f00e92135631c14c
Jeremy Melnyk <jeremyjm91@gmail.com>
2017-09-16 20:56:36 -0400

    Front end icons working.

--- cyberus/container/src/app/app.component.ts
+++ cyberus/container/src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+// Visual Recognition
+import { VisualRecognitionService } from './visual-recognition.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,17 +10,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Welcome to CyberusAI';
-      sources: Array<Object>;
+  sources: Array<Object>;
 
-    constructor() {
-        this.sources = [
-            {
-                src: "assets/videos/VIRAT_S_010000_00_000000_000165.mp4",
-                type: "video/mp4"
-            }
-        ];
-    }
+  constructor(private visualRecognitionService: VisualRecognitionService) {
+    this.sources = [
+      {
+        src: "assets/videos/VIRAT_S_010000_00_000000_000165.mp4",
+        type: "video/mp4"
+      },
+      {
+        src: "assets/videos/VIRAT_S_000201_00_000018_000380.mp4",
+        type: "video/mp4"
+      }
+    ];
+  }
 
-    ngOnInit() {
-    }
+  public testWatson() {
+    this.visualRecognitionService.classify("assets/putin.jpg");
+  }
+
+  ngOnInit() {
+  }
 }
---
+++ cyberus/container/src/app/visual-recognition.service.ts
@@ -0,0 +1,21 @@
+//import { VisualRecognitionV3 } from 'watson-developer-cloud';
+//import * as fs from 'fs';
+
+import { Injectable } from '@angular/core';
+//import * as API_KEY from '../API_KEY';
+
+@Injectable()
+export class VisualRecognitionService {
+    private visualRecognition;
+
+    constructor() {
+    }
+
+    public classify(imagePath: string): void {
+    }
+
+    ngOnInit() {
+    }
+}
+
+
